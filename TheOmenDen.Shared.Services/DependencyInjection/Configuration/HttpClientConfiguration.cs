0ec413b5870555848f59317c9e5738f32fa70b68
Andrew Diego Beers <andrew.d.beers@gmail.com>
2022-07-26 04:14:51 -0400

    Add project files.

---
+++ TheOmenDen.Shared.Services/DependencyInjection/Configuration/HttpClientConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace TheOmenDen.Shared.Services.DependencyInjection.Configuration;
+
+/// <summary>
+/// Configuration class for injected <see cref="IHttpClientFactory"/> 
+/// </summary>
+public sealed class HttpClientConfiguration
+{
+    /// <summary>
+    /// The name of the client we're trying to register
+    /// </summary>
+    public String Name { get; set; }
+
+    /// <summary>
+    /// The client's base address
+    /// </summary>
+    public String BaseAddress { get; set; }
+}
+
